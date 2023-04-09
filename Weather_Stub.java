fcf4f0cf599f35f0cdae687ed6ca3f6031b00dc4
SephMon <kevinosullivan25@gmail.com>
2023-04-09 14:18:08 +0100

    added variables

--- Weather_Stub.java
+++ Weather_Stub.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Weather_Stub{
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static final String NAME[] = {"gerry"};
+    private static final String PASS[] = {"cat"};
+    private static final String DAYS_OF_THE_WEEK[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+    private static final boolean keepGoing = true;
+    private static final int MAX_LOGIN_ATTEMPTS = 3;
+    private static final int NUM_NUMS = 7;
+    private static int[] temps = new int[8];
+    private static int[] rainFall = new int[8];
+    private static int[] humidity = new int[8];
+    private static int total;
+    private static int max = Integer.MIN_VALUE;
+    private static int min = Integer.MAX_VALUE;
+    private static int choice;
     public static void main(String[] args){
-        System.out.println("hi mate! hows it going! im good dgdfgdf fhdfhfgh");
+        
     }
 }
